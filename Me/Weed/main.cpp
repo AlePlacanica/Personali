b3c2dd8b6da9b891ecdc6ea22d8ca7007e65268e
AlePlacanica <alexplacanica@gmail.com>
2026-02-14 05:22:11 +0100

    a

--- Me/Weed/main.cpp
+++ Me/Weed/main.cpp
@@ -6,18 +6,42 @@
 #include <sstream>
 #include <ctime> 
 #include <unistd.h>
-#include <termios.h>
+
+#ifdef __APPLE__
+#include <mach-o/dyld.h>
+#include <climits>
+#endif
 
 using namespace std;
-const string PATH_CSV = "./database_weed.csv";
-const string PATH_LOG = "./registro_vendite.txt";
 
-// Palette Retro-Minimal
+// Ottiene la cartella dove si trova l'eseguibile
+string getExecutableDir() {
+#ifdef __APPLE__
+    uint32_t size = PATH_MAX;
+    char path[PATH_MAX];
+    _NSGetExecutablePath(path, &size);
+    string fullPath(path);
+    return fullPath.substr(0, fullPath.find_last_of("/"));
+#else
+    // Su a-shell e altre piattaforme, usa la cartella corrente
+    return ".";
+#endif
+}
+
+// Percorsi relativi alla cartella dell'eseguibile
+string BASE_DIR = getExecutableDir();
+const string PATH_CSV = BASE_DIR + "/database_weed.csv";
+const string PATH_LOG = BASE_DIR + "/registro_vendite.txt";
+
+// Codice di accesso per funzioni nascoste
+const string ADMIN_CODE = "1234";
+
+// Palette Retro-Neon
 const string RESET  = "\033[0m";
-const string AMBER  = "\033[38;5;214m"; // Arancione Fosforo per estetica
-const string GREEN  = "\033[1;32m";    // Verde Profitto
-const string RED    = "\033[1;31m";    // Rosso Perdita
-const string DIM    = "\033[2m";       // Testo secondario
+const string AMBER  = "\033[38;5;214m"; 
+const string GREEN  = "\033[1;32m";    
+const string RED    = "\033[1;31m";    
+const string DIM    = "\033[2m";       
 
 struct Carico {
     string fornitore;
@@ -59,25 +83,7 @@ void mostraLogo() {
     cout << "  | | | |   __|   __|   \\   |     |  _  |   | |  _  |   __|" << endl;
     cout << "  | | | |   __|   __|  |  |  | | | |     | | | |     |  |  |" << endl;
     cout << "  |_____|_____|_____|___/   |_|_|_|__|__|_|___|__|__|_____|" << endl;
-    cout << " ........................................... system v1.12 .." << RESET << endl;
-}
-
-bool checkEscape() {
-    termios oldt, newt;
-    tcgetattr(STDIN_FILENO, &oldt);
-    newt = oldt;
-    newt.c_lflag &= ~(ICANON | ECHO);
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-    
-    int ch = getchar();
-    
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-    
-    if (ch == 27) {
-        return true;
-    }
-    ungetc(ch, stdin);
-    return false;
+    cout << " ........................................... system v1.15 .." << RESET << endl;
 }
 
 void salvaTutto() {
@@ -99,6 +105,7 @@ void salvaTutto() {
 }
 
 void caricaDati() {
+    magazzino.clear(); // Pulisci i dati vecchi
     ifstream file(PATH_CSV);
     if (!file.is_open()) return;
     string riga;
@@ -118,6 +125,37 @@ void caricaDati() {
     file.close();
 }
 
+void editaConVim(const string& filePath) {
+    system("clear");
+    cout << AMBER << ">> OPENING_VIM_EDITOR..." << RESET << endl;
+    string comando = "vim \"" + filePath + "\"";
+    system(comando.c_str());
+    system("clear");
+    caricaDati();
+    cout << AMBER << ">> FILE_RELOADED_FROM_DISK." << RESET << endl;
+    cin.ignore(); cin.get();
+}
+
+bool verificaAutenticazione() {
+    system("clear");
+    cout << AMBER << ">> RESTRICTED_AREA - ACCESS_REQUIRED" << RESET << endl;
+    cout << DIM << " Enter authorization code: " << RESET;
+    string codice;
+    cin >> codice;
+    
+    if (codice == ADMIN_CODE) {
+        system("clear");
+        cout << GREEN << ">> ACCESS_GRANTED." << RESET << endl;
+        usleep(500000);
+        return true;
+    } else {
+        system("clear");
+        cout << RED << ">> ACCESS_DENIED - INVALID_CODE." << RESET << endl;
+        usleep(800000);
+        return false;
+    }
+}
+
 int main() {
     caricaDati();
     int scelta;
@@ -126,13 +164,13 @@ int main() {
         cout << "\n " << AMBER << "[01]" << RESET << " New Input Sequence" << endl;
         cout << " " << AMBER << "[02]" << RESET << " Execute Discharge" << endl;
         cout << " " << AMBER << "[03]" << RESET << " Access Ledger File" << endl;
-        cout << " " << AMBER << "[04]" << RESET << " Read Chrono Log" << endl;
+        cout << " " << AMBER << "[04]" << RESET << " Read Chrono Log (Stream)" << endl;
         cout << " " << AMBER << "[05]" << RESET << " Shutdown System" << endl;
         cout << DIM << "\n root@hawkins_lab > " << RESET;
         cin >> scelta;
 
         if (scelta == 1) {
-            mostraLogo();
+            system("clear");
             Carico c;
             cout << AMBER << ">> INCOMING_SHIPMENT_DATA" << RESET << "\n\n";
             cout << " Source: "; cin >> c.fornitore;
@@ -147,7 +185,7 @@ int main() {
             cin.ignore(); cin.get();
         } 
         else if (scelta == 2) {
-            mostraLogo();
+            system("clear");
             if (magazzino.empty()) continue;
             cout << AMBER << ">> DISCHARGE_SEQUENCE" << RESET << "\n\n";
             cout << " Entry ID: "; int idx; cin >> idx;
@@ -176,15 +214,13 @@ int main() {
             for (int i = 0; i < (int)magazzino.size(); i++) {
                 string pCol;
                 if (magazzino[i].utileAttuale < 0) pCol = RED;
-                else if (magazzino[i].utileAttuale == 0) pCol = AMBER; // Ambra per il pareggio
+                else if (magazzino[i].utileAttuale == 0) pCol = AMBER;
                 else pCol = GREEN;
 
                 cout << " " << left << setw(4) << i << setw(15) << magazzino[i].fornitore << setw(12) << magazzino[i].dataCarico
                      << setw(10) << fixed << setprecision(2) << magazzino[i].getGrammiVenduti()
                      << setw(10) << magazzino[i].grammiRimanenti;
-                
                 cout << pCol << setw(12) << magazzino[i].utileAttuale << RESET;
-                
                 cout << setw(10) << magazzino[i].getGrammiPerPareggio() << endl;
             }
             cout << DIM << " ........................................................................." << RESET << endl;
@@ -193,21 +229,38 @@ int main() {
         }
         else if (scelta == 4) {
             mostraLogo();
-            cout << AMBER << ">> LOADING_CHRONO_LOG_STREAM..." << RESET << "\n" << endl;
-            // 'cat' stampa il contenuto del file direttamente nel terminale
-            string comando = "cat " + PATH_LOG; 
-            system(comando.c_str()); 
-            
+            cout << AMBER << ">> OPENING_CHRONO_LOG_STREAM..." << RESET << "\n" << endl;
+            string comando = "cat " + PATH_LOG;
+            system(comando.c_str());
             cout << DIM << "\n..........................................." << RESET;
             cout << DIM << "\n[Enter] to return to system" << RESET;
             cin.ignore(); cin.get();
         }
+        else if (scelta == 0) {
+            if (verificaAutenticazione()) {
+                system("clear");
+                cout << AMBER << ">> ADMIN_MENU_UNLOCKED" << RESET << "\n\n";
+                cout << " " << AMBER << "[1]" << RESET << " Edit Database (Vim)" << endl;
+                cout << " " << AMBER << "[2]" << RESET << " Edit Log File (Vim)" << endl;
+                cout << " " << AMBER << "[0]" << RESET << " Return to main menu" << endl;
+                cout << DIM << "\n root@hawkins_lab > " << RESET;
+                int admin_choice;
+                cin >> admin_choice;
+                
+                if (admin_choice == 1) {
+                    editaConVim(PATH_CSV);
+                } else if (admin_choice == 2) {
+                    editaConVim(PATH_LOG);
+                }
+            }
+        }
     } while (scelta != 5);
 
     mostraLogo();
-    cout << AMBER << " WRITING_TO_DISK..." << RESET << endl;
+    cout << AMBER << " COMMIT_TO_DISK: WRITING_LOGS..." << RESET << endl;
     salvaTutto();
-    usleep(800000);
-    cout << DIM << " SESSION_TERMINATED." << RESET << endl;
+    usleep(900000);
+    cout << DIM << " SESSION_TERMINATED. DISK_IDLE." << RESET << endl;
     return 0;
 }
+
